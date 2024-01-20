ffcd1c0feab996d970b55a7a5985010d93cce807
unknown <you@example.cojadhavprathamesh957@gmail.com>
2024-01-20 17:46:46 +0530

    ui tweaks

--- src/components/Restaurant/Menu/Accordion.js
+++ src/components/Restaurant/Menu/Accordion.js
@@ -1,7 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
 import MenuItemList from "./MenuItemList";
-
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
@@ -10,8 +8,8 @@ const Accordion = ({ data, isOpen, onToggle }) => {
   const itemCount = data.itemCards.length;
 
   return (
-    <div className="px-4 md:px-8 lg:px-16 w-full text-slate-900">
-      <div className="md:w-1/2 mx-auto mb-4 bg-white shadow-md p-4 rounded-xl">
+    <div className="px-4 md:px-28 lg:px-16 w-full text-slate-900">
+      <div className="lg:w-1/2 mx-auto mb-4 bg-white shadow-md p-4 rounded-xl">
         <div
           className="flex justify-between items-center cursor-pointer"
           onClick={onToggle}
