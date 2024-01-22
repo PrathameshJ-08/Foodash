42b090353da17f6cec1202885e581b24fae37bf8
unknown <you@example.cojadhavprathamesh957@gmail.com>
2024-01-22 17:09:40 +0530

    ui

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -56,8 +56,8 @@ const SignUp = ({
   return (
     <>
       {showSignUp && (
-        <div className="p-0 m-0 right-0 top-0 absolute w-[418px] md:h-[46.45rem]">
-          <div className="z-50 pointer-events-auto md:h-[46.45rem] max-w-[418px] right-0 p-10 bg-slate-100 rounded-md shadow-md absolute">
+        <div className="p-0 m-0 right-0 top-0 absolute w-[418px] h-screen">
+          <div className="z-50 pointer-events-auto  max-w-[418px] h-screen right-0 p-10 bg-slate-100 rounded-md shadow-md absolute">
             <p
               className="absolute top-4 left-4 cursor-pointer"
               onClick={handleClose}
