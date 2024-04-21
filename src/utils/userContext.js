e8b3f84d8832b16cb50bd63c46090134461ed448
Prathamesh <jadhavprathamesh957@gmail.com>
2024-04-21 13:42:30 +0530

    minor tweaks

--- src/utils/userContext.js
+++ src/utils/userContext.js
@@ -1,19 +1,35 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const UserContext = createContext();
 
 export const useUserContext = () => {
   const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
   return context;
 };
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+      setIsAuth(true);
+    }
+  }, []);
 
   const updateUser = (userData) => {
     setUser(userData);
     setIsAuth(userData !== null);
+
+    if (userData) {
+      localStorage.setItem("user", JSON.stringify(userData));
+    } else {
+      localStorage.removeItem("user");
+    }
   };
 
   return (
